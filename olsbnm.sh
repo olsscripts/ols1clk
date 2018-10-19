7b9b0faa03253ed5fc4218f458b21b1bbcd200c8
xpressos <chrisdavies2000@gmail.com>
2018-10-19 02:40:11 +0100

    Update olsbnm.sh

--- olsbnm.sh
+++ olsbnm.sh
@@ -435,7 +435,13 @@ function install_wordpress
         rm -f /root/ioncube_loaders_lin_x86-64.tar.gz
 	    rm -rf /root/ioncube
         systemctl restart lsws
-        cd -
+        #cd -
+	  #SSL INSTALL#
+    systemctl stop lsws
+    wget -P /usr/bin https://dl.eff.org/certbot-auto
+    chmod +x /usr/bin/certbot-auto
+    /usr/bin/certbot-auto certonly --standalone -n --preferred-challenges http --agree-tos --email $EMAIL -d $SITEDOMAIN 
+    systemctl start lsws
 	
     else
         echoY "$WORDPRESSPATH exists, will use it."
@@ -1060,12 +1066,7 @@ END
         echoR "$SERVER_ROOT/conf/httpd_config.conf is missing, it seems that something went wrong during openlitespeed installation."
         ALLERRORS=1
     fi
-    #SSL INSTALL#
-    systemctl stop lsws
-    wget -P /usr/bin https://dl.eff.org/certbot-auto
-    chmod +x /usr/bin/certbot-auto
-    /usr/bin/certbot-auto certonly --standalone -n --preferred-challenges http --agree-tos --email $EMAIL -d $SITEDOMAIN 
-    systemctl start lsws
+  
 }
 
 function getCurStatus
