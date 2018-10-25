adf51b27d859a772feb412dcf315190a15363ed9
xpressos <chrisdavies2000@gmail.com>
2018-10-25 20:07:40 +0100

    Update olsbnm.sh

--- olsbnm.sh
+++ olsbnm.sh
@@ -570,13 +570,22 @@ END
     fi
     
     #mysql_secure_installation
-mysql -u root -p"$ROOTPASSWORD"
-DELETE FROM mysql.user WHERE User='';
-DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-DROP DATABASE IF EXISTS test;
-DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
-FLUSH PRIVILEGES;
-EXIT
+#mysql -u root -p"$ROOTPASSWORD"
+#DELETE FROM mysql.user WHERE User='';
+#DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+#DROP DATABASE IF EXISTS test;
+#DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
+#FLUSH PRIVILEGES;
+#EXIT
+
+mysql --user=root -p"$ROOTPASSWORD" <<_EOF_
+  DELETE FROM mysql.user WHERE User='';
+  DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+  DROP DATABASE IF EXISTS test;
+  DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
+  FLUSH PRIVILEGES;
+  EXIT
+_EOF_
     
     mysqladmin -uroot password $ROOTPASSWORD
     if [ $? = 0 ] ; then
